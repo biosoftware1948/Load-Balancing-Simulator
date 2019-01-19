662ca29e05cb88eb957368b689f2d9e2e4d83a8a
mfin23 <mfindlay@scu.edu>
2019-01-19 14:02:55 -0800

    device hardware

--- compute_node.py
+++ compute_node.py
@@ -4,10 +4,16 @@ class DeviceState(Enum):
     BUSY = 0
     FREE = 1
 
+class DeviceHardware(object):
+    def __init__(cpu, mem):
+        self.__cpu = cpu
+        self.__mem = mem
+
 class ComputeNode(object):
-    def __init__(self):
+    def __init__(self, device_hardware):
         self.state = DeviceState.FREE
-    
+        self.device_hardware = device_hardware
+        
     def assign_job(self):
         self.state = DeviceState.BUSY
     
