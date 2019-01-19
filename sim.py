84e92707749045951f35757209cec288c48cdfee
rherriott <44274807+rherriott@users.noreply.github.com>
2019-01-19 13:28:21 -0800

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,37 +1,48 @@
+#these are as much intended to flesh out ideas as be the basis for future functional code
 
-
-
-
-#Computer (worker node)
+#Computer (worker node, created by the node generator)
 ##Pre-cond: its qualities (Power, memory)
-##Post-cond: a complete node which can receive workload units and complete them, logging stats as it goe
+##Post-cond: a complete node which can receive workload units and complete them, logging stats as it goes
 Class ComputeNode:
-  def __init__(self):
+  def __init__(self,cpu,mem):
+    self.cpu = cpu #"power" more than actual CPU cycles/time
+    self.mem = mem
   
 #Computer holding class (bag of computers with creation/management funcs)
 ##Pre-cond: parameters of a number of computers and their qualities
 ##Post-cond: the computer holding object which contains the specified computers, and a log message 
-Class NodeBag:
-  def __init__(self):
-    
-    
+Class NodeGen:
+  def __init__(self,number,qualities): #need to add something to make sure this is formatted correctly
+    self.count = number
+    self.nodes = set();
+    for i in range(number):
+      nodes = nodes | ComputeNode(qualities(i).cpu,qualities(i).mem) #I made this a bag but that can obvs change
     
 #Workload (work, stores the qualities of a job a computer will have to do)
 ##Pre-cond: job qualities
 ##Post-cond: object that defines a workload
 Class Work:
-  def __init__(self):
-    
+  def __init__(self,diff,length): #"difficulty" being fraction of 1 time period (turn) to compute 1 unit of length
+    self.diff = diff
+    self.length = length
     
     
 #WorkloadGen (generates work)
 ##Pre-cond: parameter sets
 ##Post-cond: work objects produced over the simulation period at 
 Class WorkloadGen:
-  def __init__(self):
+  def __init__(self,parameters):
+    #need to add a check for good formatting
+    self.parameters = parameters
   
 #Balancer (algorithm implementor)
 ##Pre-cond: workload generator’s output stream, computer holding object’s input stream, algorithm
 ##Post-cond: object which runs every turn to allocate work to worker computers, logs actions
 Class Balancer:
-  def __init__(self):
+  def __init__(self,workgen,nodebag,algorithm) #the alg gets passed in here
+    self.workgen = workgen
+    self.nodes = list()
+    for x in nodebag:
+      self.nodes = self.nodes + x
+    self.alg = algorithm
+    
