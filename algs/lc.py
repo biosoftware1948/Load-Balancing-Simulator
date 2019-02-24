99db09c1353c8b027fc807bc4bc7cf9915239f62
Christopher Yates <cmyates48@gmail.com>
2019-02-24 13:15:49 -0800

    updated least connections. it won't work

--- algs/lc.py
+++ algs/lc.py
@@ -1,10 +1,12 @@
 # least connection algorithm
+#   - alg won't work! it assumes every node has a list property, "connections," that keeps track of the current jobs on node
+#   - our current implementation assumes every node has only 1 job, not multiple
 
 # assuming 1 job and entire cluster is sent from load balancer
 def leastConnections(job, cluster):
     cur_node = cluster.nodes[0]
     for node in cluster.nodes:
-        if node.connections < cur_node.connections:
+        if len(node.connections) < len(cur_node.connections):
             least_connected = node.connections
 
     cur_node.assignjob()
