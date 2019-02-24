85b597eb8761d4aa933ebd984e0054b9f3630ff8
Christopher Yates <cmyates48@gmail.com>
2019-02-24 13:11:31 -0800

    created least connections algorithm

--- algs/lc.py
+++ algs/lc.py
@@ -1,1 +1,13 @@
 # least connection algorithm
+
+# assuming 1 job and entire cluster is sent from load balancer
+def leastConnections(job, cluster):
+    cur_node = cluster.nodes[0]
+    for node in cluster.nodes:
+        if node.connections < cur_node.connections:
+            least_connected = node.connections
+
+    cur_node.assignjob()
+    # assign_job makes the node state = busy, but what is keeping track of how long its busy for?
+    # every job has a cycle number, but how will node be aware of that
+
