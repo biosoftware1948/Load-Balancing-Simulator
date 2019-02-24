cb8635c292aacb1a80d01c0777842dd22ba85540
Christopher Yates <cmyates48@gmail.com>
2019-02-24 12:57:27 -0800

    Updated random.py

--- algs/random.py
+++ algs/random.py
@@ -1,1 +1,12 @@
 # random algorithm
+from random import randint
+
+# assuming 1 job and entire cluster is sent from load balancer
+def random(job, cluster):
+    while (True):
+        cur_node = cluster.nodes[randint(0,cluster.node_count-1)]
+        if cur_node.state == 1:
+            cur_node.assign_job
+            # assign_job makes the node state = busy, but what is keeping track of how long its busy for?
+            # every job has a cycle number, but how will node be aware of that
+            break
