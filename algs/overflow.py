a711d7fd7cfdd892e0ebd7605b21e384bc170b6a
rherriott <44274807+rherriott@users.noreply.github.com>
2019-02-24 19:21:56 -0800

    Update overflow.py

--- algs/overflow.py
+++ algs/overflow.py
@@ -1,27 +1,37 @@
 class OverflowLoadBalancer(LoadBalancer):
     def run_load_balancing():
-        if not cluster.nodes:
+        attr = true
+        if not cluster:
           print("OverflowLoadBalancer(): failure, no cluster")
           os.flush()
           return true
-        if not cluster.nodes[0]:
+        if not cluster.nodes:
           print("OverflowLoadBalancer(): failure, no nodes")
           os.flush()
           return true
         if not cluster.nodes[0].attributes:
-          print("OverflowLoadBalancer(): failure, no attributes")
+          print("OverflowLoadBalancer(): failure, no attributes, using node order as priority")
           os.flush()
-          return true
+          attr = false
         if not cluster.nodes[0].attributes.priority:
-          print("OverflowLoadBalancer(): failure, no priority attributes")
+          print("OverflowLoadBalancer(): failure, no priority attributes, using node order as priority")
           os.flush()
-          return true
+          attr = false
         
-        while (True):
-            cur_node = cluster.nodes[randint(0,cluster.node_count-1)]
-            if cur_node.state == 1:
+        
+        if !attr:
+          while (True):
+            for cur_node in cluster.nodes
+              if cur_node.state == 1:
                 cur_node.assign_job()
                 # assign_job makes the node state = busy, but what is keeping track of how long its busy for?
                 # every job has a cycle number, but how will node be aware of that
                 break
-
+        else:
+          while (True):
+            for cur_node in sorted(cluster.nodes, key = attribute.priority)
+               if cur_node.state == 1:
+                cur_node.assign_job()
+                # assign_job makes the node state = busy, but what is keeping track of how long its busy for?
+                # every job has a cycle number, but how will node be aware of that
+                break
