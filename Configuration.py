0faca7ccc692298a61a97ae816cf059360779a5c
mfin23 <mfindlay@scu.edu>
2019-03-09 17:28:35 -0800

    Added cool way to do configs

---
+++ Configuration.py
@@ -0,0 +1,73 @@
+import json
+
+class Config(object):
+    def __init__(self):
+        self.config = None
+
+    def __load_json(self):
+        with open("config.json") as f:
+            self.config =  json.load(f)
+
+    def get_property(self, property_name):
+        if self.config == None:
+            self.__load_json()
+
+        key_list = property_name.split("/")
+
+        val = self.config
+        for key in key_list:
+            try:
+                val = val[key]
+            except KeyError:
+                return None
+        return val
+
+class SimulationConfig(Config):
+    #@property
+    def sim_runtime(self):
+        return self.get_property("simulation_config/sim_runtime")
+
+class JobsConfig(Config):
+    @property
+    def num_jobs(self):
+        return self.get_property("jobs_config/num_jobs")
+
+    @property
+    def max_runtime(self):
+        return self.get_property("jobs_config/max_runtime")
+
+    @property
+    def max_memory(self):
+        return self.get_property("jobs_config/max_memory")
+
+class ClusterConfig(Config):
+    @property
+    def num_nodes(self):
+        return self.get_property("cluster_config/num_nodes")
+
+    @property
+    def homogenous(self):
+        return self.get_property("cluster_config/homogenous")
+
+    @property
+    def non_homogenous_config(self):
+        return self.get_property("cluster_config/non_homogenous_config")
+
+class AlgorithmsConfig(Config):
+    @property
+    def algorithms(self):
+        return self.get_property("algorithms")
+
+class GlobalConfig():
+    def __init__(self):
+        self.ClusterConfig = ClusterConfig()
+        self.JobsConfig = JobsConfig()
+        self.SimulationConfig = SimulationConfig()
+        self.AlgorithmsConfig = AlgorithmsConfig()
+
+
+
+
+
+
+
