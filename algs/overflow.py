e62c4a961b2d75d4541da42bfec0d1ddb25b7f2d
rherriott <44274807+rherriott@users.noreply.github.com>
2019-03-10 13:29:03 -0700

    Update overflow.py
    switched to try/except

--- algs/overflow.py
+++ algs/overflow.py
@@ -5,23 +5,42 @@ from compute_node import DeviceHardware
 class OverflowLoadBalancer(LoadBalancer):
     def run_load_balancing(self):
         attr = True
-        if not self.cluster:
-          print("OverflowLoadBalancer(): failure, no cluster")
-          #os.flush()
-          return true
-        if not self.cluster.nodes:
-          print("OverflowLoadBalancer(): failure, no nodes")
-          #os.flush()
-          return True
-        if not self.cluster.nodes[0].attributes:
-          print("OverflowLoadBalancer(): failure, no attributes, using node order as priority")
-          #os.flush()
-          attr = False
-        if not self.cluster.nodes[0].attributes.priority:
-          print("OverflowLoadBalancer(): failure, no priority attributes, using node order as priority")
-          #os.flush()
-          attr = False
-        
+        #if not self.cluster:
+        #  print("OverflowLoadBalancer(): failure, no cluster")
+        #  #os.flush()
+        #  return true
+        try:
+            self.cluster
+        except NameError:
+            print("OverflowLoadBalancer(): failure, no cluster")
+            return true
+        #if not self.cluster.nodes:
+        #  print("OverflowLoadBalancer(): failure, no nodes")
+        #  #os.flush()
+        #  return True
+        try:
+            self.cluster.nodes
+        except NameError:
+            print("OverflowLoadBalancer(): failure, no nodes")
+            return true
+        #if not self.cluster.nodes[0].attributes:
+        #  print("OverflowLoadBalancer(): failure, no attributes, using node order as priority")
+        #  #os.flush()
+        #  attr = False
+        try:
+            self.cluster.nodes[0].attributes
+        except NameError:
+            print("OverflowLoadBalancer(): failure, no attributes, using node order as priority")
+            attr = False
+        #if not self.cluster.nodes[0].attributes.priority:
+        #  print("OverflowLoadBalancer(): failure, no priority attributes, using node order as priority")
+        #  #os.flush()
+        #  attr = False
+        try:
+            self.cluster.nodes[0].attributes.priority
+        except NameError:
+            print("OverflowLoadBalancer(): failure, no priority attributes, using node order as priority")
+            attr = False        
         
         if not attr:
           while (True):
