668ddd37d524d0b2d9088bba43e952de95ff8aea
Colin <rodwell.colin@gmail.com>
2019-03-16 16:56:14 -0700

    change config to add in free-only random load balancer and round robin load balancer

--- load_balancer.py
+++ load_balancer.py
@@ -1,8 +1,6 @@
 import abc
 import random
 
-DEBUG = True
-
 class LoadBalancer(object):
     def __init__(self, output_interface_count):
         self.__output_interface_count = output_interface_count
@@ -42,51 +40,17 @@ class LoadBalancer(object):
 
 class RandomLoadBalancer(LoadBalancer):
     def run_load_balancing(self):
-        while (not self.JOB_QUEUE.isEmpty()):
-            index = random.randint(0, self.cluster.get_num_nodes()-1)      
+        #while (True):
+        index = random.randint(0, self.cluster.get_num_nodes()-1)
+        if (not self.JOB_QUEUE.isEmpty()):       
             job = self.get_next_job()
             self.assign_job(index, job)
-
             #cur_node = self.cluster.get_node(index)
             # if cur_node.state == 1:
             #     cur_node.assign_job()
             #     # assign_job makes the node state = busy, but what is keeping track of how long its busy for?
             #     # every job has a cycle number, but how will node be aware of that
             #     break
-
-#like random, but will not assign to a busy node. if all nodes busy, it will just wait
-class RandomFreeLoadBalancer(LoadBalancer):
-    def run_load_balancing(self):
-        while (not self.JOB_QUEUE.isEmpty()):
-            emptyNodes = [i for i in range(self.cluster.get_num_nodes()) if self.cluster.get_node(i).is_free]
-            if len(emptyNodes) == 0:
-                if DEBUG:
-                    print("no nodes available, waiting")
-                return  #no free node - wait for one to be free
-            index = random.choice(emptyNodes)      
-            job = self.get_next_job()
-            self.assign_job(index, job)
-
-#Round Robin Algorithm. Still have to check for busy nodes and assign accordingly          
-class RoundRobinLoadBalancer(LoadBalancer):
-
-    def assign_cluster(self, cluster): 
-        self.cluster = cluster
-        self.__output_interfaces = cluster.nodes
-        self.num_nodes = len(self.__output_interfaces)
-
-        self.counter = 0
-
-    def run_load_balancing(self):
-        curr_node = self.__output_interfaces[self.counter]
-        job = self.get_next_job()
-        curr_node.assign_job(job)
-
-    def increment_counter(self):
-        self.counter += 1
-        if self.counter >= self.num_nodes:
-            self.counter = 0
-
 
 class Queue(object):
     def __init__(self):
